cd77771d274d8f2fe00d154dca74514f8d136989
kevlarpixels <kevingaguilar1@gmail.com>
2018-08-24 21:12:07 -0700

    Pseudo code update

---
+++ asset/javascript/game.js
@@ -0,0 +1,16 @@
+// Pseudocode
+
+//input randomNumber into randomNumber id, name let cpuRandomNumber
+var cpuRandom = Math.floor
+//attach let to html id
+//input let for win div, name let win
+//input let for losses div, name let losses
+//input randomNumber into userTotal id, name let userTotalCount
+//attach let to html
+//attach randomNumber to crystal 1-4, name let crystalOne, etc..
+//attach win html id to win let
+//attach losses html id to win let
+//set up start of game by showing initial state of variables
+//set up win of game by showing state of variables
+//set up loss of game by showing state of variable
+//set up state of each crystal img on click. Include if else statement
